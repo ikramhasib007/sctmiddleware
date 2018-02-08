5c8ce01b7431cda0454a6d2d6ada90b9ea7c3264
ikramhasib007 <ikramhasib007@gmail.com>
2018-02-08 23:25:58 +0600

    add contains_array function and complete 1 depth array iteration

--- IsArray.php
+++ IsArray.php
@@ -21,7 +21,7 @@ class IsArray {
     public function encode_process($flag = false) {
         $array = array();
         if (!$flag) {
-            // with old index
+            // with all index
             foreach ($this->data as $data_v) {
                 $array[] = $this->array_with_all_index($data_v);
             }
@@ -38,41 +38,13 @@ class IsArray {
     private function array_with_all_index(array $arr) {
         if(!$this->keys)
             return $arr;
-        foreach ($this->keys as $old_key => $new_key) {
-            //echo 'Old key: '.$old_key.' New key: '.print_r($new_key, true).'<br>';
-            if(is_array($new_key)){
-//                echo '<pre>';
-//                var_dump($new_key);
-                //$depthArr = array();
-                foreach ($arr as $arr_key => $arr_value){
-                    if(is_array($arr_value)){
-                        echo '<pre>';
-                        print_r($arr_value);
-                        echo '</pre>';
-                        foreach ($new_key as $key => $value) {
-                            echo 'Old key: '.$key.' New key: '.print_r($value, true).'<br>';
-                            if (array_key_exists($key, $arr_value)) {
-                                $arr_value[$key] = $arr_value[$value];
-                                unset($arr_value[$value]);
-                            }
-                        }
-                    }
-                }
-                $arr[$old_key] = $new_key;
-                echo '<pre>';
-                var_dump($arr);
-                echo '</pre>';
-                //unset($arr[$old_key]);
-                //echo 'I m array. ok!! ';
-            } else {
-                if (array_key_exists($old_key, $arr)) {
-                    $arr[$new_key] = $arr[$old_key];
-                    unset($arr[$old_key]);
-                }
-            }
-            
-        }
-        return $arr;
+         foreach ($this->keys as $old_key => $new_key) {
+             if (array_key_exists($old_key, $arr)) {
+                 $arr[$new_key] = $arr[$old_key];
+                 unset($arr[$old_key]);
+             }
+         }
+         return $arr;
     }
 
     private function array_with_new_index(array $arr) {
@@ -92,7 +64,7 @@ class IsArray {
         if($this->keys)
             $this->keys = array_flip($this->keys);
         if (!$flag) {
-            // with old index
+            // with all index
             foreach ($this->data as $data_v) {
                 $array[] = $this->array_with_all_index($data_v);
             }
@@ -104,6 +76,15 @@ class IsArray {
         }
         $this->data = $array;
         return $this->data;
+    }
+    
+    public function contains_array($array){
+        foreach($array as $value){
+            if(is_array($value)) {
+              return true;
+            }
+        }
+        return false;
     }
 
     public function __toString() {
