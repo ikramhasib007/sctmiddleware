59869c9ac101acc304fb8f28cbd73b24e0069613
ikramhasib007 <ikramhasib007@gmail.com>
2018-02-13 02:01:28 +0600

    associative array checking complete.

--- IsArray.php
+++ IsArray.php
@@ -24,24 +24,65 @@ class IsArray {
     public function processed($flag = false) {
         $array = array();
         $keys = $this->keys;
+        $array_depth = '';
         if (!$flag) {
             //** with all index **/
-            foreach ($this->data as $data_v) {
-//                echo $this->array_depth($data_v);
+            if (!$this->array_assoc_keys($this->data)) {
+                foreach ($this->data as $data_v) {
+//            echo count($this->data);
+//                echo '<pre>';
+//                print_r($this->data);
+//                echo '</pre>';
 //                exit();
-                if ($this->contains_array($data_v)) {
-                    $array[] = $this->array_with_array_indexing($data_v, $keys);
+//                echo $this->array_depth($data_v);
+
+
+                    if ($this->contains_array($data_v)) {
+//                    $array_depth = $this->array_depth($data_v);
+//                    foreach ($data_v as $arr_key => $value){
+//                        if(is_array($value)){
+//                            echo $arr_key;
+//                            echo '<pre>';
+//                            print_r($value);
+////                            exit();
+//                        }
+//                    }
+//                    echo $array_depth;
+//                    echo '<pre>';
+//                    print_r($data_v);
+//                    exit();
+//                    for($count = 2; $count < $array_depth; $count++){
+//                        
+//                    }
+                        $array[] = $this->array_with_array_indexing($data_v, $keys);
+                    } else {
+                        $array[] = $this->array_with_all_index($data_v, $keys);
+                    }
+                }
+            } else {
+                //** non indexed array */
+                if ($this->contains_array($this->data)) {
+                    $array[] = $this->array_with_array_indexing($this->data, $keys);
                 } else {
-                    $array[] = $this->array_with_all_index($data_v, $keys);
+                    $array[] = $this->array_with_all_index($this->data, $keys);
                 }
             }
         } else {
             //** without old index, only new index **/
-            foreach ($this->data as $data_v) {
-                if ($this->contains_array($data_v)) {
-                    $array[] = $this->array_with_array_new_indexing($data_v, $keys);
+            if (!$this->array_assoc_keys($this->data)) {
+                foreach ($this->data as $data_v) {
+                    if ($this->contains_array($data_v)) {
+                        $array[] = $this->array_with_array_new_indexing($data_v, $keys);
+                    } else {
+                        $array[] = $this->array_with_new_index($data_v, $keys);
+                    }
+                }
+            } else {
+                //** non indexed array */
+                if ($this->contains_array($this->data)) {
+                    $array[] = $this->array_with_array_new_indexing($this->data, $keys);
                 } else {
-                    $array[] = $this->array_with_new_index($data_v, $keys);
+                    $array[] = $this->array_with_new_index($this->data, $keys);
                 }
             }
         }
@@ -57,6 +98,9 @@ class IsArray {
         $indexedArray = FALSE;
         $numberOfIndexedArray = '';
         $array = $this->array_with_all_index($arr, $keys);
+//        echo '<pre>';
+//        print_r($array);
+//        exit();
         foreach ($arr as $o_key => $a) {
             if (is_array($a)) {
                 if (array_key_exists(0, $a)) {
@@ -254,7 +298,7 @@ class IsArray {
     private function replace_keys_with_array($oldKey, $newKey, array $input, array $replaced) {
         $return = array();
         foreach ($input as $key => $value) {
-            if ($key === $oldKey){
+            if ($key === $oldKey) {
                 $key = $newKey;
                 $value = $replaced;
             }
@@ -270,13 +314,17 @@ class IsArray {
         $max_depth = 1;
         foreach ($array as $value) {
             if (is_array($value)) {
-                $depth = array_depth($value) + 1;
+                $depth = $this->array_depth($value) + 1;
                 if ($depth > $max_depth) {
                     $max_depth = $depth;
                 }
             }
         }
         return $max_depth;
+    }
+    
+    private function array_assoc_keys(array $array) {
+        return count(array_filter(array_keys($array), 'is_string')) > 0;
     }
 
     public function __toString() {
